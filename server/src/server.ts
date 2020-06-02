f5974d41e63e1829981f6f41dbbf95f377346e39
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-02 09:31:49 -0300

    Configurada rotas de listagem, busca e criação para consulta no Chocolatey.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,42 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-    console.log("Listagem de usuários");
+app.use(express.json());
 
-    response.json([
-        'Diego',
-        'Rodrigo',
-        'Carla',
-        'Emanuel',
-        "Mayk"
-    ]);
+const users = [
+    'Diego',
+    'Rodrigo',
+    'Carla',
+    'Emanuel',
+    "Mayk"
+];
+
+app.get("/users", (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+    const data = request.body;
+    console.log(data);
+    
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+    return response.json(user);
 });
 
 app.listen(3333, function() {
