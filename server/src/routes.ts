29a35a7b4c4118dce48c28ab863ceae4bf615ec5
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-02 16:09:40 -0300

    Adicinada rotas das imagens.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,42 +1,20 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get("/users", (request, response) => {
-    return response.json({ message: "Hello World" });
+routes.get("/items", async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(serializedItems);
 });
 
 export default routes;
-
-// const users = [
-//     'Diego',
-//     'Rodrigo',
-//     'Carla',
-//     'Emanuel',
-//     "Mayk"
-// ];
-// app.get("/users", (request, response) => {
-//     const search = String(request.query.search);
-
-//     const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-//     return response.json(filteredUsers);
-// });
-// app.get("/users/:id", (request, response) => {
-//     const id = Number(request.params.id);
-
-//     const user = users[id];
-
-//     return response.json(user);
-// });
-
-// app.post("/users", (request, response) => {
-//     const data = request.body;
-//     console.log(data);
-    
-//     const user = {
-//         name: data.name,
-//         email: data.email
-//     }
-//     return response.json(user);
-// });
