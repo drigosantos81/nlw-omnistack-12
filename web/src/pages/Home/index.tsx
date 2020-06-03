c4de95f36efcdfa0d29f68df7c11a59731d7f3a8
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-03 13:42:41 -0300

    Incluido formulário de cadaastro de pontos de coleta com mapa integrado.

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
